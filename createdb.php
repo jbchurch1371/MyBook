2e20bc7210da00b4e36de5630c6aa7b50e5bb2ba
jbchurch1371 <134632279+jbchurch1371@users.noreply.github.com>
2023-06-04 13:31:12 -0500

    Create createdb.php

---
+++ createdb.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset = "utf-8">
+  <title>Create MySQL DB on Azure</title>
+  </head>
+  <body>
+    <?php
+    // replace user and password
+    $host = 
+    $user = "jbchurch1@ccis.edu";
+    $password = "Brennan123!";
+    // connect
+    $conn = mysqli_connect($host, $user, $password);
+    if($conn)
+      echo "<p>Connection is good</p>";
+    // drop database
+    $query = "DROP DATABASE IF EXISTS visitorDB";
+    mysqli_query($conn, $query);
+    // create sql query
+    $query = "CREATE DATABASE visitorDB";
+    // execute
+    if(mysqli_query($conn, $query))
+      echo "<p>Database created</p>";
+    // database created, need tables
+    mysqli_select_db($conn, "visitorDB");
+    $query = "CREATE TABLE visitor
+    (
+    visitorid INTEGER AUTO_INCREMENT,
+    visitorName VARCHAR(100) NOT NULL,
+    visitTime TIMESTAMP DEFAULT NOW(),
+    PRIMARY KEY(visitorid)
+    )";
+    if(mysqli_query($conn, $query))
+      echo "<p> Table created </p>";
+    // close
+    mysqli_close($conn);
+    ?>
+  </body>
+</html>
